8dfb86b317b5683d64ee7888ade6ccb226f8b19b
luc <luctunechi45@gmail.com>
2018-07-10 13:07:42 +0200

    new change

--- src/ComponentsPricing/ContactUs.js
+++ src/ComponentsPricing/ContactUs.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
-import './ContactUs.css';
+import React, { Component } from "react"
+import "./ContactUs.css"
 
-class ContactUs extends Component{
-    render(){
-        
-        return(
-        <div className="">
-            <div className="main-contact-div bg-prices">
-                <div className="container">
-                    <p className="contact-us-text-parent">Get Started now</p>
-                    <div className="col-md-8 offset-md-2">
-                        <p className="contact-us-text-child">During the first 6 months rollout, SAVE will be exclusively operational for NGOs (practitioners). If you’re an NGO working with Saving Groups in Rwanda, do not hesitate to try SAVE today for 30 days free trial.</p>
-                        <button type="button" className="btn btn-secondary btn-contact-us"><span className="btn-text2">START FREE TRIAL</span></button>
-                    </div>
-                </div>
+class ContactUs extends Component {
+  render() {
+    return (
+      <div className="">
+        <div className="main-contact-div bg-prices text-center">
+          <div className="container">
+            <p className="contact-us-text-parent">Get Started now</p>
+            <div className="col-md-8 offset-md-2">
+              <p className="contact-us-text-child">
+                During the first 6 months rollout, SAVE will be exclusively
+                operational for NGOs (practitioners). If you’re an NGO working
+                with Saving Groups in Rwanda, do not hesitate to try SAVE today
+                for 30 days free trial.
+              </p>
+              <button type="button" className="btn  btn-contact-us">
+                <span className="btn-text2">START FREE TRIAL</span>
+              </button>
             </div>
+          </div>
         </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
-
 export default ContactUs
-
-
-
--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -1,74 +1,94 @@
-import React, { Component } from 'react';
-import './HowItWorks.css';
-import ReactDOM from 'react-dom';
-import YoutubeIcon from '../images/youtube.svg';
-import SaveFlow from '../images/how-desktop.svg';
-import SaveFlowPhone from '../images/how-it-works-phone.svg';
-import ModalVideo from 'react-modal-video';
-import { Element ,  animateScroll  } from 'react-scroll'
+import React, { Component } from "react"
+import "./HowItWorks.css"
+import ReactDOM from "react-dom"
+import YoutubeIcon from "../images/youtube.svg"
+import SaveFlow from "../images/how-desktop.svg"
+import SaveFlowPhone from "../images/how-it-works-phone.svg"
+import ModalVideo from "react-modal-video"
+import { Element, animateScroll } from "react-scroll"
 
+class HowItWorks extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isOpen: false
+    }
+    this.openModal = this.openModal.bind(this)
+  }
 
-class HowItWorks extends Component{
-    
-    constructor () {
-        super()
-        this.state = {
-            isOpen: false
-        }
-        this.openModal = this.openModal.bind(this)
-    }
-    
-    openModal () {
-        this.setState({isOpen: true})
-    }
-    
-    
-    render(){
-        return(
-        <Element name="test1" className="element">
-            <div className="main-background">
-                <div className="jumbotron jumbo2 jumbotron-fluid">
-                    <p className="text-center text-1">HOW IT WORKS</p>
-                    <div className="d-none d-sm-block text-center img-padding">
-                        <img className="img-fluid base-home-saving" src={SaveFlow} alt="flow" />
-                    </div>
-                    
-                    <div className="d-block d-sm-none text-center img-padding">
-                        <img className="img-fluid " src={SaveFlowPhone} alt="flow" />
-                    </div>
-                    <div className="row-3">
-                        <p className="d-none d-sm-block text-center text-phone">Collective saving made cashless!</p>
-                        <p className="d-block d-sm-none text-center text-phone">Collective saving made cashless!</p>
-                    </div>
-                    <div className="d-none d-sm-block col-md-8 offset-md-2 text-center subtitle-content">
-                        <p className="home-subtitle subtitle2">SAVE will primarily focus on Saving Groups and will treat both the individual and
-                            members of a Saving Groups with the same level of priority.
-                            This will be achieved through three integrated components: SAVE Web, SAVE Collector (Android) and SAVE USSD.
-                        SAVE is here to empower the member of each Saving Group.</p>
-                    </div>
-                    
-                    <div className="d-block d-sm-none text-center subtitle-content">
-                        <p className="home-subtitle subtitle2">SAVE will primarily focus on Saving Groups and will treat both<br/> the individual and
-                            members of a Saving Groups with the same level of priority.
-                            This will be achieved through three integrated  components: SAVE Web, SAVE  Collector (Android) and SAVE USSD.
-                        SAVE is here to empower the member of each Saving Group.</p>
-                    </div>
-                    
-                    <div className="youtube-icon">
-                        <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId='vDfEp-UImYE' onClose={() => this.setState({isOpen: false})} />
-                            <img src={YoutubeIcon} onClick={this.openModal} className="mx-auto d-block ytb-icon" data-toggle="tooltip" data-placement="top" title="Open Video" alt="youtube logo" />
-                        </div>
-                    </div>
-                </div>
-            </Element>
-            )
-        }
-    }
-    
-     
-    
- export default HowItWorks
-    
-    
-    
-     
+  openModal() {
+    this.setState({ isOpen: true })
+  }
+
+  render() {
+    return (
+      <Element name="test1" className="element">
+        <div className="main-background">
+          <div className="jumbo2 jumbotron-fluid">
+            <p className="text-center text-1">HOW IT WORKS</p>
+            <div className="d-none d-sm-block text-center img-padding">
+              <img
+                className="img-fluid base-home-saving"
+                src={SaveFlow}
+                alt="flow"
+              />
+            </div>
+
+            <div className="d-block d-sm-none text-center img-padding">
+              <img className="img-fluid " src={SaveFlowPhone} alt="flow" />
+            </div>
+            <div className="row-3">
+              <p className="d-none d-sm-block text-center text-phone">
+                Collective saving made cashless!
+              </p>
+              <p className="d-block d-sm-none text-center text-phone">
+                Collective saving made cashless!
+              </p>
+            </div>
+            <div className="d-none d-sm-block col-md-8 offset-md-2 text-center subtitle-content">
+              <p className="home-subtitle subtitle2">
+                SAVE will primarily focus on Saving Groups and will treat both
+                the individual and members of a Saving Groups with the same
+                level of priority. This will be achieved through three
+                integrated components: SAVE Web, SAVE Collector (Android) and
+                SAVE USSD. SAVE is here to empower the member of each Saving
+                Group.
+              </p>
+            </div>
+
+            <div className="d-block d-sm-none text-center subtitle-content">
+              <p className="home-subtitle subtitle2">
+                SAVE will primarily focus on Saving Groups and will treat both<br />{" "}
+                the individual and members of a Saving Groups with the same
+                level of priority. This will be achieved through three
+                integrated components: SAVE Web, SAVE Collector (Android) and
+                SAVE USSD. SAVE is here to empower the member of each Saving
+                Group.
+              </p>
+            </div>
+
+            <div className="youtube-icon">
+              <ModalVideo
+                channel="youtube"
+                isOpen={this.state.isOpen}
+                videoId="vDfEp-UImYE"
+                onClose={() => this.setState({ isOpen: false })}
+              />
+              <img
+                src={YoutubeIcon}
+                onClick={this.openModal}
+                className="mx-auto d-block ytb-icon"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="Open Video"
+                alt="youtube logo"
+              />
+            </div>
+          </div>
+        </div>
+      </Element>
+    )
+  }
+}
+
+export default HowItWorks
